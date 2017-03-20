4f992f31a576a6b6e3529a32ec100f0deb320478
ecoron <ronald.schmidt@zu-web.de>
2017-03-20 23:32:52 +0100

    start adding example_markovi

--- serpscrap/markovi.py
+++ serpscrap/markovi.py
@@ -7,14 +7,16 @@ class Markovi():
     def __init__(self, config=None):
         self.config = config
 
-    def generate(self, text, chars=140):
+    def generate(self, text, state_size=3, chars=140):
         # Build the model.
-        text_model = markovify.Text(text)
+        text_model = markovify.Text(text, state_size)
         
         # Print five randomly-generated sentences
         for _ in range(5):
-            print(text_model.make_sentence())
+            print(text_model.make_sentence(tries=100, max_overlap_ratio=0.7, max_overlap_total=15))
+            print()
         
         # Print three randomly-generated sentences of no more than 140 characters
         for _ in range(3):
             print(text_model.make_short_sentence(chars))
+            print()

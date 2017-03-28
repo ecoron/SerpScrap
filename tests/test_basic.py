4bec365d975597b1a6e096ec1e0123cda1948676
ecoron <ronald.schmidt@zu-web.de>
2017-03-28 10:01:14 +0200

    update test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-import serpscrap
+from serpscrap.config import Config
+from serpscrap.serpscrap import SerpScrap
 
 
 class TestClass:
     def test_simple(self):
         keywords = ['computer since']
-        config = serpscrap.Config()
+        config = Config()
         config.set('scrape_urls', False)
-        scrap = serpscrap.SerpScrap()
+        scrap = SerpScrap()
         scrap.init(config=config.get(), keywords=keywords)
         results = scrap.run()
 
+        assert len(config.get()) == 13
         assert len(results) > 0
         assert len(results[0]) > 0

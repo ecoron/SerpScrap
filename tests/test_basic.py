3308a95939f8f80d043dee4c0b1f0cb5e3235935
ecoron <ronald.schmidt@zu-web.de>
2017-04-12 22:20:11 +0200

    update test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -26,6 +26,6 @@ class TestClass:
         scrap.init(config=config.get(), keywords=keywords)
         results = scrap.run()
 
-        assert len(config.get()) == 24
+        assert len(config.get()) == 26
         assert len(results) > 0
         assert len(results[0]) > 0

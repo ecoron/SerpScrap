52e983d8054feba7f001279d04f43d190de95b1a
ecoron <ronald.schmidt@zu-web.de>
2018-08-26 09:52:45 +0200

    experimental install chromedriver

--- install_chrome.sh
+++ install_chrome.sh
@@ -12,7 +12,7 @@ SELENIUM_SUBDIR=$(echo "$SELENIUM_STANDALONE_VERSION" | cut -d"." -f-2)
 # Remove existing downloads and binaries so we can start from scratch.
 sudo apt-get remove google-chrome-stable
 rm ~/chromedriver_linux64.zip
-sudo rm /usr/local/bin/chromedriver
+# sudo rm /usr/local/bin/chromedriver
 
 # Install dependencies.
 sudo apt-get install -y unzip openjdk-8-jre-headless xvfb libxi6 libgconf-2-4
@@ -24,9 +24,9 @@ sudo apt-get -y update
 sudo apt-get -y install google-chrome-stable
 
 # Install ChromeDriver.
-wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/
-unzip ~/chromedriver_linux64.zip -d ~/
-rm ~/chromedriver_linux64.zip
-sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
-sudo chown root:root /usr/local/bin/chromedriver
-sudo chmod 0755 /usr/local/bin/chromedriver
+# wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/
+# unzip ~/chromedriver_linux64.zip -d ~/
+# rm ~/chromedriver_linux64.zip
+# sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
+# sudo chown root:root /usr/local/bin/chromedriver
+# sudo chmod 0755 /usr/local/bin/chromedriver

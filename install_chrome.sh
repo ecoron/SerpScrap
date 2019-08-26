d89e552ec5344270a2b2aea2caaf87dc71e82140
Ronald Schmidt <ronald.schmidt@sparwelt.de>
2019-08-26 11:04:37 +0200

    try to solve travis chrome install issues

--- install_chrome.sh
+++ install_chrome.sh
@@ -18,8 +18,8 @@ rm ~/chromedriver_linux64.zip
 sudo apt-get install -y unzip openjdk-8-jre-headless xvfb libxi6 libgconf-2-4
 
 # Install Chrome.
-sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
-# sudo echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get -y update
 sudo apt-get -y install google-chrome-stable
 

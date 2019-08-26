9469b5c5b27be09ab454da9ce52410c97cd2a7af
Ronald Schmidt <ronald.schmidt@zu-web.de>
2019-08-26 15:07:57 +0200

    rollback selenium version

--- install_chrome.sh
+++ install_chrome.sh
@@ -6,7 +6,7 @@
 
 # Versions
 CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
-SELENIUM_STANDALONE_VERSION=3.14.1
+SELENIUM_STANDALONE_VERSION=3.4.0
 SELENIUM_SUBDIR=$(echo "$SELENIUM_STANDALONE_VERSION" | cut -d"." -f-2)
 
 # Remove existing downloads and binaries so we can start from scratch.
@@ -15,7 +15,8 @@ rm ~/chromedriver_linux64.zip
 # sudo rm /usr/local/bin/chromedriver
 
 # Install dependencies.
-sudo apt-get install -y unzip openjdk-8-jre-headless xvfb libxi6 libgconf-2-4
+sudo apt-get update
+apt-get install -y unzip openjdk-8-jre-headless xvfb libxi6 libgconf-2-4
 
 # Install Chrome.
 curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
